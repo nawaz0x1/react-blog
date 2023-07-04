aed001595fc7d3a773888d1df6f1ebfddb9af2b8
Nawaz <nawazhaider60@gmail.com>
2023-07-04 13:39:51 +0600

    fixed css

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
             />
             <a
               href="#"
-              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 "
             >
               <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
